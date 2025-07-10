abaaa75bc6a58bfe63f24f4505178c289459dec3
Fernando Castro <jf3r123@gmail.com>
2025-07-09 19:45:22 -0600

    avances

--- src/app.js
+++ src/app.js
@@ -33,8 +33,20 @@ ioServer.on("connection",(socket) => socketRouter(socket,ioServer));
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
+console.log(FRONTEND)
+//app.use(cors({ origin: [FRONTEND,TEST], optionsSuccessStatus: 200 }));
+const allowedOrigins = [FRONTEND, 'http://192.168.1.12:3003'];
 
-app.use(cors({ origin: [FRONTEND,TEST], optionsSuccessStatus: 200 }));
+app.use(cors({
+    origin: function (origin, callback) {
+        console.log(origin,allowedOrigins);
+      if (allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    }
+  }));
   
 app.use(environment === "PRODUCCTION" ? "" : "/api",require("./router/routes.js"));
 
